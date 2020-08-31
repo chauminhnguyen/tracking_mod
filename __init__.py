a70f637d71711f9e1b6eb7e4e765e27501251997
Nguyễn Minh Châu <18520519@gm.uit.edu.vn>
2020-08-31 20:23:17 +0700

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -5,7 +5,7 @@ __all__ = ['DeepSort', 'build_tracker']
 
 
 def build_tracker(cfg, max_dist, max_nms, max_iou, max_age, use_cuda):
-    return DeepSort(model_dir, 
+    return DeepSort(model_path=cfg.DEEPSORT.REID_CKPT, 
                 max_dist=max_dist, min_confidence=cfg.DEEPSORT.MIN_CONFIDENCE, 
                 nms_max_overlap=max_nms, max_iou_distance=max_iou, 
                 max_age=max_age, n_init=cfg.DEEPSORT.N_INIT, nn_budget=cfg.DEEPSORT.NN_BUDGET, use_cuda=use_cuda)
